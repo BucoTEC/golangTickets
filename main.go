8fdd4fcb609cead152fdd3122f427a3ec08535ca
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-12 12:07:58 +0100

    configuring first go app

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, welcome to our booking aplication")
+	fmt.Println("Get your tickets here")
+
+	var conferenceName = "Go Conferecne"
+	fmt.Println(conferenceName)
+}
