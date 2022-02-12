c1e2481d49c6d59a274ad22845834784891ac65d
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-12 12:19:34 +0100

    foramted string

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, welcome to our booking aplication")
-	fmt.Println("Get your tickets here")
-
 	var conferenceName = "Go Conferecne"
-	fmt.Println(conferenceName)
+	const conferenceTickets = 50 
+	var remainingTickets = 50
+	fmt.Printf("Hello, welcome to our %v booking aplication!\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets, and %v are still avalible\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here.")
+
 }
