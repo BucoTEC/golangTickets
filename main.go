4565edfe95f3d13228e4d100087c4bc48404e67e
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-12 13:41:22 +0100

    configured loop

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conferecne"
@@ -9,7 +12,11 @@ func main() {
 	fmt.Printf("Hello, welcome to our %v booking aplication!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets, and %v are still avalible\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here.")
+	firstNames := []string{}
 
+for{
+
+	
 	var firstName string
 	var lastName string
 	var email string
@@ -27,9 +34,18 @@ func main() {
 	fmt.Scan(&tickets)
 	
 	bookings = append(bookings,  firstName + " " + lastName) 
-
+	
 	remainingTickets = remainingTickets - tickets
 	fmt.Printf("Hello %v %v your email adress is %v. You have booked %v tickets\n",firstName, lastName, email, tickets)
 	fmt.Printf("%v tickets remaining\n", remainingTickets)
 	fmt.Println(bookings)
+
+
+	for _,booking := range bookings {
+		var names  = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	} 
+		fmt.Println(firstNames)
+}
+
 }
